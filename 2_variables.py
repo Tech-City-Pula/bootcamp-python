d88600c9d9591b9c04544ff41bb96d071ce263b0
voiys <matej2714@gmail.com>
2023-11-08 07:48:53 +0100

    make comments more readable

--- 2_variables.py
+++ 2_variables.py
@@ -1,5 +1,14 @@
-age = 27 # int
-name = "Matej" # string
-money = 420.69 # float
-is_programmer = True # boolean
-problems = None # NoneType
+# int
+age = 27
+
+# string
+name = "Matej"
+
+# float
+money = 420.69 
+
+# boolean
+is_programmer = True 
+
+# NoneType
+problems = None
