59d3e5477af0b23484b9d18b64bb6bff1e8d24e8
voiys <matej2714@gmail.com>
2023-11-08 19:55:06 +0100

    predavanje 2

--- 3_conditions/exercise.py
+++ 3_conditions/exercise.py
@@ -1,4 +1,16 @@
 # Exercise 1: Write an if statement to check if a number stored in a variable is even or odd. Print an appropriate message for each case.
+number = 4
 
+if number % 2 == 0:
+    print(f"{number} is even")
+else:
+    print(f"{number} is odd")
 
 # Exercise 2: Ask the user to input a word and check if that word is "Python". If it is, print "You guessed the right word!", otherwise print "Try again!".
+
+word = input("enter a word\n")
+
+if word == "Python":
+    print("You guessed the right word!")
+else:
+    print("Try again!")
