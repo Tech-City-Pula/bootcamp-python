e9de5e8568352b4760ba9daf21a8d848f4b4a54a
voiys <matej2714@gmail.com>
2023-11-08 07:59:06 +0100

    added data structures file

---
+++ 5_data_structures.py
@@ -0,0 +1,21 @@
+# list
+basket = ["apples", "grapes"]
+
+# set
+unique_items_in_basket = set(basket)
+
+# tuple
+pula_coordinates = (44.8666, 13.8496)
+
+# dict
+player = {
+    "username": "hackerman",
+    "level": 1337,
+    "items": ["sword", "shield"],
+    "stats": {
+        "health_points": 100,
+        "mana": 72,
+        "strength": 10,
+        "intelligence": 25
+    }
+}
